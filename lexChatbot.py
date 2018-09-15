955f5550ebfcbf3178d55e22d1e60d9f7a533f1c
mayank <mayankkumaryadav06@gmail.com>
2018-09-16 03:12:02 +0530

    Inital Commit

---
+++ lexChatbot.py
@@ -0,0 +1,23 @@
+    try :
+        name = event["currentIntent"]["slots"]["Relation"].title()
+        #print event["currentIntent"]["slots"]
+        if name == "Recruiter" or name == "recruiter" :
+            l_response = "Good to see you here. Mayank is talented DevOps Engineer who also likes coding in python. Would you like to know more?"
+        elif name == "Friend" or name == "friend" :
+            l_response = "Nice. Good to meet his friend. He indeed miss you. Do you have his contact number?"
+        response = {
+                    "dialogAction":
+                        {
+                         "fulfillmentState":"Fulfilled",
+                         "type":"Close","message":
+                            {
+                              "contentType":"PlainText",
+                              "content": l_response
+                            }
+                        }
+                    }
+    except :
+        pass
+    
+    try: 
+    return response
